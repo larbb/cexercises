10f79fd04ae32313e08bef9be9a029f8330e9d96
Lewis R <thefarcry118@gmail.com>
2013-07-13 15:17:25 +0100

    getchar function without logical operators (&& ||)

---
+++ precedence.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define LIM 100
+
+int main() 
+{
+    int i, c;
+    char s[LIM];
+
+    i = c = 0;
+    while (i < LIM -1) {
+        c = getchar();
+        if (c != EOF) 
+            if (c != '\n')
+            s[i++] = c;
+        }
+    s[i] = '\0';
+return 0;
+}
+
+
